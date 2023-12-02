40e38d84c93cda2314f1e94c5fd6543e3117e74e
Jussi Känsälä <jussikan@users.noreply.github.com>
2023-12-02 22:06:43 +0200

    compile main.py into bytecode in build script. updated PLAN.

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ cp -Rp appskeleton "build/${APP_NAME}.app"
 
 mkdir -p "build/${APP_NAME}.app/Contents/Resources/lib"
 # TODO use just 'python3'.
-python3.9 -m compileall lib
+python3.9 -m compileall lib main.py
 find lib/__pycache__ -type f -name '*.pyc' -exec mv {} "build/${APP_NAME}.app/Contents/Resources/lib" \;
-cp main.py "build/${APP_NAME}.app/Contents/Resources/"
+mv main.pyc "build/${APP_NAME}.app/Contents/Resources/"
 
 cp bin/{find-duplicates.sh,mark-duplicates.sh,delete-duplicates.sh} "build/${APP_NAME}.app/Contents/Resources/bin/"
 
